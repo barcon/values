156412ded4bc60189fcbb18f504d66f136191985
Daniel Rodrigo <12259909+barcon@users.noreply.github.com>
2025-03-25 21:07:28 +0100

    Working on values

--- src/values_vector.cpp
+++ src/values_vector.cpp
@@ -63,6 +63,136 @@ namespace values
         value_ = value;
     }
 
+    ValueVector1DPtr CreateValueVector1D(const Vector& value)
+    {
+        auto res = ValueVector1D::Create();
+
+        res->SetValue(value);
+
+        return res;
+    }
+    ValueVector1DPtr CreateValueVector1D(const Vector& value, String name, String key)
+    {
+        auto res = ValueVector1D::Create();
+
+        res->SetName(name);
+        res->SetKey(key);
+        res->SetValue(value);
+
+        return res;
+    }
+    IVector1DPtr CastToVector1D(IValuePtr value)
+    {
+        return std::dynamic_pointer_cast<IVector1D>(value);
+    }
+    ValueVector1DPtr ValueVector1D::ValueVector1D::Create()
+    {
+        class MakeSharedEnabler : public ValueVector1D
+        {
+        };
+
+        auto res = std::make_shared<MakeSharedEnabler>();
+
+        return res;
+    }
+    Type ValueVector1D::GetType() const
+    {
+        return type_;
+    }
+    const String& ValueVector1D::GetName() const
+    {
+        return name_;
+    }
+    const String& ValueVector1D::GetKey() const
+    {
+        return key_;
+    }
+    Vector ValueVector1D::GetValue(Scalar x) const
+    {
+        return value_;
+    }
+    Vector ValueVector1D::GetValue(const Vector& point) const
+    {
+        return value_;
+    }
+    void ValueVector1D::SetName(const String& name)
+    {
+        name_ = name;
+    }
+    void ValueVector1D::SetKey(const String& key)
+    {
+        key_ = key;
+    }
+    void ValueVector1D::SetValue(const Vector& value)
+    {
+        value_ = value;
+    }
+
+    ValueVector2DPtr CreateValueVector2D(const Vector& value)
+    {
+        auto res = ValueVector2D::Create();
+
+        res->SetValue(value);
+
+        return res;
+    }
+    ValueVector2DPtr CreateValueVector2D(const Vector& value, String name, String key)
+    {
+        auto res = ValueVector2D::Create();
+
+        res->SetName(name);
+        res->SetKey(key);
+        res->SetValue(value);
+
+        return res;
+    }
+    IVector2DPtr CastToVector2D(IValuePtr value)
+    {
+        return std::dynamic_pointer_cast<IVector2D>(value);
+    }
+    ValueVector2DPtr ValueVector2D::ValueVector2D::Create()
+    {
+        class MakeSharedEnabler : public ValueVector2D
+        {
+        };
+
+        auto res = std::make_shared<MakeSharedEnabler>();
+
+        return res;
+    }
+    Type ValueVector2D::GetType() const
+    {
+        return type_;
+    }
+    const String& ValueVector2D::GetName() const
+    {
+        return name_;
+    }
+    const String& ValueVector2D::GetKey() const
+    {
+        return key_;
+    }
+    Vector ValueVector2D::GetValue(Scalar x, Scalar y) const
+    {
+        return value_;
+    }
+    Vector ValueVector2D::GetValue(const Vector& point) const
+    {
+        return value_;
+    }
+    void ValueVector2D::SetName(const String& name)
+    {
+        name_ = name;
+    }
+    void ValueVector2D::SetKey(const String& key)
+    {
+        key_ = key;
+    }
+    void ValueVector2D::SetValue(const Vector& value)
+    {
+        value_ = value;
+    }
+
     ValueVector3DPtr CreateValueVector3D(const Vector& value)
     {
         auto res = ValueVector3D::Create();
@@ -81,7 +211,7 @@ namespace values
 
         return res;
     }
-    IVector3DPtr CastToVector3d(IValuePtr value)
+    IVector3DPtr CastToVector3D(IValuePtr value)
     {
         return std::dynamic_pointer_cast<IVector3D>(value);
     }
--- src/values_vector.hpp
+++ src/values_vector.hpp
@@ -40,6 +40,78 @@ namespace values
 		Type type_{ value_vector };
 	};
 
+	class ValueVector1D;
+	using ValueVector1DPtr = std::shared_ptr<ValueVector1D>;
+	using ConstValueVector1DPtr = std::shared_ptr< const ValueVector1D >;
+
+	ValueVector1DPtr CreateValueVector1D(const Vector& value);
+	ValueVector1DPtr CreateValueVector1D(const Vector& value, String name, String key);
+
+	IVector1DPtr CastToVector1D(IValuePtr value);
+
+	class ValueVector1D : public IVector1D
+	{
+	public:
+		virtual ~ValueVector1D() = default;
+
+		static ValueVector1DPtr Create();
+
+		Type GetType() const override;
+		const String& GetName() const override;
+		const String& GetKey() const override;
+		Vector GetValue(Scalar x) const override;
+		Vector GetValue(const Vector& point) const override;
+
+		void SetName(const String& name) override;
+		void SetKey(const String& key) override;
+		void SetValue(const Vector& value);
+
+	protected:
+		ValueVector1D() = default;
+
+		String name_{};
+		String key_{};
+		Vector value_;
+
+		Type type_{ value_vector1D };
+	};
+
+	class ValueVector2D;
+	using ValueVector2DPtr = std::shared_ptr<ValueVector2D>;
+	using ConstValueVector2DPtr = std::shared_ptr< const ValueVector2D >;
+
+	ValueVector2DPtr CreateValueVector2D(const Vector& value);
+	ValueVector2DPtr CreateValueVector2D(const Vector& value, String name, String key);
+
+	IVector2DPtr CastToVector2D(IValuePtr value);
+
+	class ValueVector2D : public IVector2D
+	{
+	public:
+		virtual ~ValueVector2D() = default;
+
+		static ValueVector2DPtr Create();
+
+		Type GetType() const override;
+		const String& GetName() const override;
+		const String& GetKey() const override;
+		Vector GetValue(Scalar x, Scalar y) const override;
+		Vector GetValue(const Vector& point) const override;
+
+		void SetName(const String& name) override;
+		void SetKey(const String& key) override;
+		void SetValue(const Vector& value);
+
+	protected:
+		ValueVector2D() = default;
+
+		String name_{};
+		String key_{};
+		Vector value_;
+
+		Type type_{ value_vector2D };
+	};
+
 	class ValueVector3D;
 	using ValueVector3DPtr = std::shared_ptr<ValueVector3D>;
 	using ConstValueVector3DPtr = std::shared_ptr< const ValueVector3D >;
@@ -47,7 +119,7 @@ namespace values
 	ValueVector3DPtr CreateValueVector3D(const Vector& value);
 	ValueVector3DPtr CreateValueVector3D(const Vector& value, String name, String key);
 
-	IVector3DPtr CastToVector3d(IValuePtr value);
+	IVector3DPtr CastToVector3D(IValuePtr value);
 
 	class ValueVector3D : public IVector3D
 	{
